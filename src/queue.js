1d807efcd0e320bc6560885be897dd68529cb370
Danil336 <barsecdebil@gmail.com>
2022-10-17 13:28:54 +0200

    feat: solved some tasks

--- src/queue.js
+++ src/queue.js
@@ -1,6 +1,6 @@
 const { NotImplementedError } = require('../extensions/index.js');
 
-// const { ListNode } = require('../extensions/list-node.js');
+const { ListNode } = require('../extensions/list-node.js');
 
 /**
  * Implement the Queue with a given interface via linked list (use ListNode extension above).
@@ -13,23 +13,76 @@ const { NotImplementedError } = require('../extensions/index.js');
  * queue.dequeue(); // returns the top element from queue and deletes it, returns 1
  * queue.getUnderlyingList() // returns { value: 3, next: null }
  */
-class Queue {
+ class Queue {
 
-  getUnderlyingList() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor() {
+    this.first = null
+    this.last = null
+    this.length = 0   
+  }
+  
+  isEmpty() {
+    return this.first == null;
   }
 
-  enqueue(/* value */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  getUnderlyingList() {
+    return this.first
+  }
+
+  enqueue(value) {
+    const newNode = new ListNode(value);
+
+    if (this.isEmpty()) {
+      this.first = newNode;
+      this.last = newNode;
+    }
+    else {
+      this.last.next = newNode;
+      this.last = newNode;
+    }
+    this.length++;
+    return this ;
   }
 
   dequeue() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+    if (this.isEmpty()) return null;
+
+    const itemToDel = this.first;
+
+    if (this.first === this.last) {
+      this.last = null;
+    }
+
+    this.first = this.first.next;
+    this.length--;
+    return itemToDel;
   }
 }
+//  class Queue {
+//   constructor() {
+//     this.elements = {};
+//     this.head = 0;
+//     this.tail = 0;
+//   }
+//   getUnderlyingList(){
+//     return this.elements[this.tail]
+//   }
+//   enqueue(element) {
+//     this.elements[this.tail] = element;
+//     this.tail++;
+//   }
+//   dequeue() {
+//     const item = this.elements[this.head];
+//     delete this.elements[this.head];
+//     this.head++;
+//     return item;
+//   }
+//   peek() {
+//     return this.elements[this.head];
+//   }
+// }
+
 
 module.exports = {
   Queue
